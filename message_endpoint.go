1f73200e74cbbf561f5651e4099176d8c270d110
zeroFruit <14wnrkim@gmail.com>
2018-11-15 12:43:42 +0900

    implements SyncSend

--- message_endpoint.go
+++ message_endpoint.go
@@ -1,14 +1,21 @@
 package swim
 
 import (
+	"errors"
 	"github.com/DE-labtory/swim/pb"
 	"github.com/gogo/protobuf/proto"
+	"github.com/it-chain/iLogger"
 
 	"time"
 )
 
+var ErrSendFailed = errors.New("Error send failed")
+var ErrSendTimeout = errors.New("Error send timeout")
+var ErrUnreachable = errors.New("Error this shouldn't reach")
+
 type MessageEndpointConfig struct {
 	EncryptionEnabled bool
+	DefaultSendTimeout time.Duration
 }
 
 // MessageEndpoint basically do receiving packet and determine
@@ -39,7 +46,7 @@ func (m *MessageEndpoint) Listen() {
 		case packet := <- m.transport.PacketCh():
 			msg, err := m.processPacket(*packet)
 			if err != nil {
-
+				iLogger.Error(nil, err.Error())
 			}
 			m.handleMessage(msg)
 
@@ -81,12 +88,55 @@ func (m *MessageEndpoint) handleMessage(msg pb.Message) {
 	// call delegate func to update members states
 }
 
+// determineSendTimeout if @timeout is given, then use this value as timeout value
+// otherwise calculate timeout value based on the awareness
+func (m *MessageEndpoint) determineSendTimeout(timeout time.Duration) time.Duration {
+	if timeout != time.Duration(0) {
+		return m.awareness.ScaleTimeout(m.config.DefaultSendTimeout)
+	}
+
+	return timeout
+}
+
 // SyncSend synchronously send message to member of addr, waits until response come back,
 // whether it is timeout or send failed, SyncSend can be used in the case of pinging to other members.
 // if @timeout is provided then set send timeout to given parameters, if not then calculate
 // timeout based on the its awareness
-func (m *MessageEndpoint) SyncSend(addr string, msg pb.Message, timeout time.Duration) (pb.Message, error) {
-	return pb.Message{}, nil
+func (m *MessageEndpoint) SyncSend(addr string, msg pb.Message, interval time.Duration) (pb.Message, error) {
+	onSucc := make(chan struct{})
+	onErr := make(chan struct{})
+
+	timeout := m.determineSendTimeout(interval)
+
+	d, err := proto.Marshal(&msg)
+	if err != nil {
+		return pb.Message{}, err
+	}
+
+	go func() {
+		_, err := m.transport.WriteTo(d, addr)
+		if err != nil {
+			iLogger.Info(nil, err.Error())
+			onErr <- struct{}{}
+			return
+		}
+		onSucc <- struct{}{}
+	}()
+
+	// start timer
+	T := time.NewTimer(timeout)
+
+	select {
+	case <- onSucc:
+
+	case <- onErr:
+		return pb.Message{}, ErrSendFailed
+
+	case <- T.C:
+		return pb.Message{}, ErrSendTimeout
+	}
+
+	return pb.Message{}, ErrUnreachable
 }
 
 // Send asynchronously send message to member of addr, don't wait until response come back,
